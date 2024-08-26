a2b482736d0ebb92fa06cc2369a97e7dac6bd02e
Nisarga-MS <84907841+Nisarga-MS@users.noreply.github.com>
2024-08-26 22:20:41 +0530

    installed all the required packages and added folder structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
-
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage";
 
 function App() {
   return (
-    <div>
-      <h1>Hi All </h1>
-    </div>
+    <Routes>
+     <Route path="/" element= {<HomePage/>} />
+    </Routes>
   );
 }
 
---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div> <h1>Im home here</h1></div>
+  )
+}
+
+export default Home
